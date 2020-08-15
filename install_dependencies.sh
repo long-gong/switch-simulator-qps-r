e95533176b5fd8283f1dbdfcc1c9cbfb8d95fe8f
Long Gong <long.github@gamil.com>
2020-08-15 10:34:52 -0700

    changed install dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,27 +1,26 @@
 #!/usr/bin/env bash
 
-set -e 
-
+set -e
 
 install_dir="$(pwd)/external"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-sudo add-apt-repository universe
-sudo apt-get update
-sudo apt-get install -y wget libz-dev 
-sudo apt-get remove libboost*
-sudo add-apt-repository ppa:mhier/libboost-latest
-sudo apt-get update
-sudo apt-get install -y libboost1.73-dev
+    sudo add-apt-repository universe
+    sudo apt-get update
+    sudo apt-get install -y wget libz-dev
+    sudo apt-get remove libboost*
+    sudo add-apt-repository ppa:mhier/libboost-latest
+    sudo apt-get update
+    sudo apt-get install -y libboost1.73-dev
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-brew install libzip 
-brew install boost@1.73
+    brew install libzip
+    brew list boost || brew install boost@1.73
 fi
 
 
-if ! [ -d ${install_dir} ]; then 
-mkdir -p "${install_dir}"
-fi 
+if ! [ -d ${install_dir} ]; then
+    mkdir -p "${install_dir}"
+fi
 
 cd /tmp
 echo "Install dependencies: lemon to ${install_dir} ..."
@@ -29,10 +28,10 @@ rm -rf lemon-1.3.1 lemon-1.3.1.tar.gz
 wget http://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz
 tar -zxvf lemon-1.3.1.tar.gz
 cd lemon-1.3.1
-mkdir build && cd build 
+mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${install_dir}
 make -j$(nproc)
-make install 
+make install
 cd /tmp
 rm -rf lemon-1.3.1 lemon-1.3.1.tar.gz
 
@@ -41,29 +40,29 @@ rm -rf HdrHistogram_c
 echo "Install dependencies: HdrHistogram_c to ${install_dir} ..."
 git clone https://github.com/HdrHistogram/HdrHistogram_c.git
 cd HdrHistogram_c
-mkdir build && cd build 
+mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${install_dir}
-make && make install 
-cd /tmp 
+make && make install
+cd /tmp
 rm -rf HdrHistogram_c
 
 rm -rf Catch2
 echo "Install dependencies: Catch2 to ${install_dir} ..."
 git clone https://github.com/catchorg/Catch2.git
 cd Catch2
-mkdir build && cd build 
+mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${install_dir}
-make && make install 
-cd /tmp 
+make && make install
+cd /tmp
 rm -rf Catch2
 
 rm -rf json
 echo "Install dependencies: json to ${install_dir} ..."
 git clone https://github.com/nlohmann/json.git
-cd json 
-mkdir build && cd build 
-cmake .. -DCMAKE_INSTALL_PREFIX=${install_dir} 
-make && make install 
-cd /tmp 
+cd json
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=${install_dir}
+make && make install
+cd /tmp
 rm -rf json
 
